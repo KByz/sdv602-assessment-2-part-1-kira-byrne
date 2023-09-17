77dd9e4b1989334a10bfe3466bd0c0e3c5470218
Kbyz <kira-byrne@live.nmit.ac.nz>
2023-09-18 08:57:36 +1200

    gui with graph DRAFT

--- gui.py
+++ gui.py
@@ -1,7 +1,21 @@
 import os.path
 import PySimpleGUI as sg
+import matplotlib as matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+t = np.arange(0, 3, .01)
+fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+
+matplotlib.use('TkAgg')
+
+def draw_graph(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
 
 data_select_column = [
     [sg.Text('Select Data to Display')],
@@ -21,6 +35,15 @@ graph_canvas = [
     [sg.Image(key='-IMAGE-')],
 ]
 
+#layout 2
+layout = [
+    [sg.Text("Graphing Prototype")],
+    [sg.Canvas(key='-CANVAS-')],
+    [sg.Button('Pie Chart'), sg.Button('Bar Chart'), sg.Button('Trend Chart'), sg.Exit()]
+]
+"""
+LAYOUT 1
+
 layout = [
     [
      sg.Column(data_select_column),
@@ -33,10 +56,28 @@ layout = [
     [sg.Button('Trend Chart')]
     
     ]
-window = sg.Window('Data Exploration Prototype', layout, size=(800, 500))
+"""
+#draw new window
+window = sg.Window(
+     'Data Exploration Prototype', 
+     layout, 
+     size=(800, 500),
+     location=(0, 0),
+     finalize=True,
+     element_justification='center',
+     font='Helvetica 18', 
+    )
 
+# Add graph to the window
+draw_graph(window['-CANVAS-'].TKCanvas, fig)
+
+event, values = window.read()
+
+window.close()
+"""
 while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
 window.close()
+"""
