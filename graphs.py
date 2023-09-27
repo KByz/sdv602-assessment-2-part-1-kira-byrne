39aba3ae7ef8e6aee2aa94bcd789b92a69401b91
Kbyz <kira-byrne@live.nmit.ac.nz>
2023-09-28 12:03:47 +1300

    Starting on subprocesses

--- graphs.py
+++ graphs.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
 def bar_chart():
@@ -14,4 +15,26 @@ def bar_chart():
     plt.title("Uptake of IT Majors")
     plt.show()
 
-plt.show(bar_chart())
+def pie_chart():
+    data = {'Personal Car': 26000, 'Bus': 14230, 'Train': 12140, 'Bicycle': 8014, 'Walk': 4410}
+    modes = list(data.keys())
+    values = list(data.values())
+
+    plt.pie(values, labels=modes, autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.axis('equal')
+    plt.title("Modes of Commute July 2023")
+    plt.show()
+
+def trend_chart():
+    Bus = {
+        "Bus_Passangers": [43191, 41042, 15293, 19183, 35193, 56180, 68113],
+        "Year": [2018, 2019, 2020, 2021, 2022, 2023, 2024]
+    }
+    df = pd.DataFrame(Bus)
+    print(df)
+
+    df.plot(x="Year", y="Bus_Passangers", kind="line")
+    plt.show()
+
+
+
--- gui.py
+++ gui.py
@@ -1,8 +1,39 @@
+"""""
+Create a GUI that can explore a set of data through three graph types.
+Using MatPlotLib to create the graphs, import data file and create a GUI.
+
+The proposed raw data will be a csv file imported from a webpage. 
+The data is a census recording of commute methods in a slection of New Zealand regions from 2017 to 2023.
+Available graphs inluce:
+    1. Pie
+    2. Bar
+    3. Trend
+
+The app must also contain a login screen that will connect to a database to verify the user.
+
+The exploration features of the app will allow users to zoom and pan the graphs, and open a chat window
+to message other collegues who are logged in. 
+
+AT STAGE ONE the repository will contain a prototype to propose the design of the app.
+This will include 
+    1. A non-functional login page
+    2. Three data exploration screens (DES) containing the graphs
+
+AT STAGE 2 the repository will contain a functional prototype of the app.
+This will include:
+    1. A functional login page
+    2. Three DES containing the graphs with data that can be manipulated in-app
+    3. Viewing features zoom and pan
+    4. A live chat function
+
+"""""
 import os.path
 import PySimpleGUI as sg
 import matplotlib as matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import graphs as grph
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
@@ -39,8 +70,11 @@ graph_canvas = [
 layout = [
     [sg.Text("Graphing Prototype")],
     [sg.Canvas(key='-CANVAS-')],
-    [sg.Button('Pie Chart'), sg.Button('Bar Chart'), sg.Button('Trend Chart'), sg.Exit()]
+    [sg.Text ("Enter Username: "), sg.Input(key="-USERNAME-", do_not_clear=False, size=(30, 1))],
+    [sg.Text ("Enter Password: "), sg.InputText('', key="-PASSWORD-", password_char='*', size=(15, 1))],
+    [sg.Button('Pie Chart'), sg.Button('Bar Chart'), sg.Button('Trend Chart'), sg.Exit(button_color='red')]
 ]
+
 """
 LAYOUT 1
 
@@ -57,11 +91,12 @@ layout = [
     
     ]
 """
+
 #draw new window
 window = sg.Window(
      'Data Exploration Prototype', 
      layout, 
-     size=(800, 500),
+     size=(800, 700),
      location=(0, 0),
      finalize=True,
      element_justification='center',
@@ -73,11 +108,16 @@ draw_graph(window['-CANVAS-'].TKCanvas, fig)
 
 event, values = window.read()
 
-window.close()
-"""
+
 while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Exit':
+        if event == 'Pie Chart':
+             plt.show(grph.pie_chart()),
+        elif event == 'Bar Chart':
+             plt.show(grph.bar_chart()),
+        elif event == 'Trend Chart':
+             plt.show(grph.trend_chart()),
+        elif  event == sg.WIN_CLOSED or event == 'Exit':
             break
+
 window.close()
-"""
